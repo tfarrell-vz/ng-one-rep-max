f3b31b9d67ebc3b4d130b41897d157115b7fbe52
tfarrell-vz <farrell@abiresearch.com>
2015-06-21 22:37:21 -0400

    Use one of the one rep max formulas: Epley formula

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = angular.module('ng-orm', []);
 app.controller('MainCtrl', function($scope) {
   $scope.calculate = function() {
-    $scope.result = $scope.weight * $scope.reps;
+    $scope.result = $scope.weight * (1 + $scope.reps/30);
   };
 });
