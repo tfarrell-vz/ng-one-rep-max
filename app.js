2b1068cbc312788e6f35ab0d8c4834972ff3b1ce
tfarrell-vz <farrell@abiresearch.com>
2015-06-22 20:05:08 -0400

    Add a reset button.

--- app.js
+++ app.js
@@ -3,4 +3,10 @@ app.controller('MainCtrl', function($scope) {
   $scope.calculate = function() {
     $scope.result = $scope.weight * (1 + $scope.reps/30);
   };
+
+  $scope.reset = function() {
+    $scope.weight = null;
+    $scope.reps = null;
+    $scope.result = null;
+  };
 });
